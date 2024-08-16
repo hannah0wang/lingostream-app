7307422355bf703f84ca83724f4485ccb41fd015
Hannah Wang <43276816+hannah0wang@users.noreply.github.com>
2024-08-15 23:13:23 -0500

    implement docker for libretranslate

---
+++ src/lib/components/LoadingContainer.svelte
@@ -0,0 +1,3 @@
+<div class="flex min-h-screen flex-col">
+    <span class="loading loading-bars loading-lg"></span>
+</div>
--- src/lib/utils/translation.ts
+++ src/lib/utils/translation.ts
@@ -1,6 +1,14 @@
 import type { TranslationProp } from "$lib";
 
+// Build and run LibreTranslate from source:
+// git clone https://github.com/LibreTranslate/LibreTranslate
+// cd LibreTranslate
+// pip install -e .
+// libretranslate [args]
 
+// # Or
+// python main.py [args]
+// Then open a web browser to http://localhost:5000
 export async function getTranslation(text: string, language: string) {
     const res = await fetch("http://127.0.0.1:5000/translate", {
         method: "POST",
